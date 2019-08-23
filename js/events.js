e7d8b114be17054a21bdb1154c644c9a4cee4f5f
Anastasia Pankina <anastaciap@ymail.com>
2019-08-23 18:05:35 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -21,7 +21,7 @@ function pressIt(){
 }
 
 function submitIt(){
-  $("form").on("submit",function(){
+  $("form").bind("submit",function(){
       alert("Your form is going to be submitted now");
   });
 }
